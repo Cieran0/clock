25c346410ab63a71f5be055083b83b559df8fc92
Cieran O'Neill <cieran999@gmail.com>
2022-10-20 19:42:59 +0100

    Second step done

--- clock.asm
+++ clock.asm
@@ -3,38 +3,76 @@ global    _start
 
 section   .text
 
-_start:     
-        mov       rax, 96
-        mov       rdi, timevalue
-        mov       rsi, timezone 
+
+get_hour:
+        mov     rax,qword[timevalue] ; qword[timevalue]  % 86400
+        mov     rcx, 86400
+        xor     rdx,rdx
+        div     rcx
+        mov     rax,rdx ; / 3600
+        mov     rcx, 3600
+        xor     rdx,rdx
+        div     rcx
+        add     rax,1 ; + 1
+        mov     byte[hour], al
+        ret
+
+get_min:
+        mov     rax,qword[timevalue] ; qword[timevalue]  % 86400
+        mov     rcx, 86400
+        xor     rdx,rdx
+        div     rcx
+        mov     rax,rdx ; % 3600
+        mov     rcx, 3600
+        xor     rdx,rdx
+        div     rcx
+        mov     rax,rdx ; /60
+        mov     rcx, 60
+        xor     rdx,rdx
+        div     rcx
+        mov     byte[min], al
+        ret
+
+print_hour:
+        mov     al, byte[hour]
+        add     al, 60
+        mov     byte[empty],al
+        mov     rax,1
+        mov     rdi,1
+        mov     rsi,empty
+        mov     rdx,2
         syscall
-        mov       r9,qword[timevalue]                 
-        mov       qword[hour],r9
-        mov       rax, qword[hour]
-        xor       rdx,rdx
-        mov       rcx,86400
-        div       rcx
-        mov       rax,rdx
-        mov       rdx,0
-        mov       rcx,3600
-        div       rcx
-        add       rax, 1
-        mov       qword[hour],rax
-        cmp       rax, 18
-        jne       notst                
-        mov       rax, 1                  ; system call for write
-        mov       rdi, 1                  ; file handle 1 is stdout
-        mov       rsi, seventeen               ; address of string to output
-        mov       rdx, 8                  ; number of bytes
-notst:
-        syscall                           ; invoke operating system to do the write
-        mov       rax, 60                 ; system call for exit
-        xor       rdi, rdi                ; exit code 0
-        syscall                           ; invoke operating system to exit
+        ret
+
+print_min:
+        mov     al, byte[min]
+        add     al, 40
+        mov     byte[empty],al
+        mov     rax,1
+        mov     rdi,1
+        mov     rsi,empty
+        mov     rdx,2
+        syscall
+        ret
+
+_start:
+        mov     rax, 96
+        mov     rdi, timevalue
+        mov     rsi, timezone
+        syscall
+        call    get_hour
+        call    print_hour
+        call    get_min
+        call    print_min
+end:
+        mov     rax, 60
+        xor     rdi, rdi
+        syscall
 
 section   .data
 
-seventeen:  db        "18",10
-hour:       db        0,0,0,0,0,0,0,0
-timevalue:  db        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0      ; note the newline at the end
-timezone:   db        0,0,0,0,0,0,0,0                      ; note the newline at the end
+empty:      db        0,10
+hour:       db        0
+min:        db        0
+timevalue:  db        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+timezone:   db        0,0,0,0,0,0,0,0
